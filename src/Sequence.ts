fbc8c4fce684f33e1c8e46b6a63532af74cf817d
saori-eth <D84da@proton.me>
2024-02-12 16:42:35 -0600

    format

--- src/Sequence.ts
+++ src/Sequence.ts
@@ -1,31 +1,31 @@
-import { ethers, parseEther } from "ethers";
-import DNToken from "./abi/DNToken.json"
-import type { Driver } from ".";
+import { ethers, parseEther } from 'ethers'
+import DNToken from './abi/DNToken.json'
+import type { Driver } from '.'
 
 export class Sequence {
-  private driver: Driver;
-  private provider: Driver["eth"]["provider"];
-  private wallet: Driver["eth"]["wallet"];
-  constructor(driver: Driver) {
-    this.driver = driver;
-    this.provider = driver.eth.provider;
-    this.wallet = driver.eth.wallet;
-    this.init();
-  }
+    private driver: Driver
+    private provider: Driver['eth']['provider']
+    private wallet: Driver['eth']['wallet']
+    constructor(driver: Driver) {
+        this.driver = driver
+        this.provider = driver.eth.provider
+        this.wallet = driver.eth.wallet
+        this.init()
+    }
 
-  private async init() {
-    await this.deployFactory();
-  }
+    private async init() {
+        await this.deployFactory()
+    }
 
-  private createFactory(): ethers.ContractFactory {
-    return new ethers.ContractFactory(
-      DNToken.abi,
-      DNToken.bytecode,
-      this.wallet
-    );
-  }
+    private createFactory(): ethers.ContractFactory {
+        return new ethers.ContractFactory(
+            DNToken.abi,
+            DNToken.bytecode,
+            this.wallet
+        )
+    }
 
-  /*
+    /*
     constructor:
         string memory name_,
         string memory symbol_,
@@ -35,26 +35,26 @@ export class Sequence {
         address initialSupplyOwner
 */
 
-  private async deployFactory() {
-    const maxMint = parseEther("1000")
-    const publicPrice = parseEther("0.1")
-    const initialTokenSupply = parseEther("50")
+    private async deployFactory() {
+        const maxMint = parseEther('1000')
+        const publicPrice = parseEther('0.1')
+        const initialTokenSupply = parseEther('50')
 
-    console.log("maxMint", maxMint)
-    console.log("publicPrice", publicPrice)
-    console.log("initialTokenSupply", initialTokenSupply)
+        console.log('maxMint', maxMint)
+        console.log('publicPrice', publicPrice)
+        console.log('initialTokenSupply', initialTokenSupply)
 
-    const factory = this.createFactory();
-    const contract = await factory.deploy(
-      "DNToken Test",
-      "DNT",
-      BigInt(maxMint),
-      BigInt(publicPrice),
-      BigInt(initialTokenSupply),
-      this.wallet.address
-    );
-    await contract.waitForDeployment();
-    const address = await contract.getAddress();
-    console.log("Deployed to:", address);
-  }
+        const factory = this.createFactory()
+        const contract = await factory.deploy(
+            'DNToken Test',
+            'DNT',
+            BigInt(maxMint),
+            BigInt(publicPrice),
+            BigInt(initialTokenSupply),
+            this.wallet.address
+        )
+        await contract.waitForDeployment()
+        const address = await contract.getAddress()
+        console.log('Deployed to:', address)
+    }
 }
