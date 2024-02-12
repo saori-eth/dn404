8930177eda17ffc980c433ab48b70d42b7ea0bfb
saori-eth <D84da@proton.me>
2024-02-12 16:02:08 -0600

    console warnings

--- src/Eth.ts
+++ src/Eth.ts
@@ -2,6 +2,12 @@ import { ethers } from 'ethers'
 import type { Driver } from './index'
 const { MODE, BASE_HTTP, LOCAL_HTTP, DEV_PK, PROD_PK } = process.env
 
+if (!MODE) console.error('MODE is not defined')
+if (!BASE_HTTP) console.error('BASE_HTTP is not defined')
+if (!LOCAL_HTTP) console.error('LOCAL_HTTP is not defined')
+if (!DEV_PK) console.error('DEV_PK is not defined')
+if (!PROD_PK) console.error('PROD_PK is not defined')
+
 export class Eth {
     private driver: Driver
     public provider: ethers.JsonRpcProvider
